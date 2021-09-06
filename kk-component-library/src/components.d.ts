cc61bd48ba955e3b731ffe754854d7cdb5f9b23f
Karan Kakroo <kakrookaran@gmail.com>
2021-09-06 18:50:56 +0000

    added Netlify.toml file

--- kk-component-library/src/components.d.ts
+++ kk-component-library/src/components.d.ts
@@ -6,6 +6,8 @@
  */
 import { HTMLStencilElement, JSXBase } from "@stencil/core/internal";
 export namespace Components {
+    interface KkButton {
+    }
     interface MyComponent {
         /**
           * The first name
@@ -22,6 +24,12 @@ export namespace Components {
     }
 }
 declare global {
+    interface HTMLKkButtonElement extends Components.KkButton, HTMLStencilElement {
+    }
+    var HTMLKkButtonElement: {
+        prototype: HTMLKkButtonElement;
+        new (): HTMLKkButtonElement;
+    };
     interface HTMLMyComponentElement extends Components.MyComponent, HTMLStencilElement {
     }
     var HTMLMyComponentElement: {
@@ -29,10 +37,13 @@ declare global {
         new (): HTMLMyComponentElement;
     };
     interface HTMLElementTagNameMap {
+        "kk-button": HTMLKkButtonElement;
         "my-component": HTMLMyComponentElement;
     }
 }
 declare namespace LocalJSX {
+    interface KkButton {
+    }
     interface MyComponent {
         /**
           * The first name
@@ -48,6 +59,7 @@ declare namespace LocalJSX {
         "middle"?: string;
     }
     interface IntrinsicElements {
+        "kk-button": KkButton;
         "my-component": MyComponent;
     }
 }
@@ -55,6 +67,7 @@ export { LocalJSX as JSX };
 declare module "@stencil/core" {
     export namespace JSX {
         interface IntrinsicElements {
+            "kk-button": LocalJSX.KkButton & JSXBase.HTMLAttributes<HTMLKkButtonElement>;
             "my-component": LocalJSX.MyComponent & JSXBase.HTMLAttributes<HTMLMyComponentElement>;
         }
     }
